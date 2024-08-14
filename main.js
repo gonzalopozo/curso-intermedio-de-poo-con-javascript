f2978c3a00273061e64a8f0bce05324e8eb03dc7
gonzalopozo <gonzalopozo2005@gmail.com>
2024-08-14 13:19:51 +0200

    Object.defineProperty

--- main.js
+++ main.js
@@ -14,11 +14,43 @@ const gonzalo = {
 // console.log(Object.getOwnPropertyNames(gonzalo));
 // console.log(Object.entries(gonzalo));
 
-console.log(Object.getOwnPropertyDescriptors(gonzalo));
 
-Object.defineProperty(gonzalo,"pruebaNASA", {
-    value: "extraterrestres",
-    enumerable: true,
-    writable: true,
-    configurable: true
-})
+// Object.defineProperty(gonzalo,"pruebaNASA", {
+    //     value: "extraterrestres",
+    //     enumerable: true,
+    //     writable: true,
+    //     configurable: true
+    // })
+    
+// Object.defineProperty(gonzalo,"navigator", {
+//     value: "brave",
+//     enumerable: false,
+//     writable: true,
+//     configurable: true
+// })
+
+// Object.defineProperty(gonzalo,"editor", {
+//     value: "VSCode",
+//     enumerable: true,
+//     writable: false,
+//     configurable: true
+// })
+
+// Object.defineProperty(gonzalo,"terminal", {
+//     value: "WSL",
+// enumerable: true,
+// writable: true,
+// configurable: false
+// })
+
+// Object.defineProperty(gonzalo,"pruebaNASA", {
+//     value: "extraterrestres",
+//     enumerable: false,
+//     writable: false,
+//     configurable: false
+// })
+
+// Object.seal(gonzalo);
+Object.freeze(gonzalo);
+
+console.log(Object.getOwnPropertyDescriptors(gonzalo));
